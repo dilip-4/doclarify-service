388d26296743a8676fd8583920627475c8b38091
dilip-4 <career.dilip.kumar@gmail.com>
2025-11-17 09:45:13 +0530

    initial commit, repo created

---
+++ src/main/java/com/doclarify/core/DoclarifyApplication.java
@@ -0,0 +1,13 @@
+package com.doclarify.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoclarifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoclarifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doclarify/core/DoclarifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doclarify.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoclarifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
